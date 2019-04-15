852abc7c5afa2db70f9766a7870f39fed0ed09fb
Zefirot <gaston.campos509@gmail.com>
2019-04-15 19:40:58 -0300

    Agregue Funciones Agregue funciones al archivo del mazo

--- Mazo.java
+++ Mazo.java
@@ -1,5 +1,56 @@
 package juego;
 
-public class Mazo {
+import java.util.Random;
 
+public class Mazo {
+	Cartas[] cartas;
+	Cartas [] cartasUsadas;
+	Random r=new Random();
+	public Mazo() {
+		this.cartas=new Cartas[50];
+		int j=0;
+		for (int i=0;i<=4;i++) {
+			for (int k=0;k<=50;k++) {
+				cartas[j++]= new Cartas(i,k);
+			}
+		}
+	}
+	public void mezclar() {
+		for (int i=0;i<=1000;i++) {
+			int k= r.nextInt(cartas.length);
+			int k1= r.nextInt(cartas.length);
+			Cartas aux= cartas[k];
+			cartas[k]=cartas[k1];
+			cartas[k1]=aux;
+		}
+	}
+	private static Cartas[] agregarAUsadas(Cartas[] lista , Cartas c) {
+		Cartas[] aux= new Cartas[lista.length+1];
+		for (int i=0;i<aux.length;i++) {
+			if (i==aux.length-1) {
+				aux[i]=c;
+			}else {
+				aux[i]=lista[i];
+			}
+		}
+		return aux;
+	}
+	private boolean estaUsada(Cartas[] lista, Cartas c) {
+		for (int i=0;i<lista.length;i++) {
+			if (lista[i]==c) {
+				return true;
+			}
+		}
+		cartasUsadas= agregarAUsadas(cartasUsadas, c);
+		return false;
+	}
+	public Cartas getCarta() {
+		int k=r.nextInt(cartas.length);
+		for (int i=0;i<=5;i++) {
+			if (estaUsada(cartasUsadas,cartas[k])==false) {
+				return cartas[k];
+			}
+		}
+		return cartas[1];
+	}
 }
